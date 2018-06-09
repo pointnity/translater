3024ea5a3f965d7dbf31cb39e6e55c53f8f8acf0
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-06-09 19:42:19 +0800

    Update test.js

--- test.js
+++ test.js
@@ -2,3 +2,4 @@ var test = require('tape'),
     KeyEncoder = require('./index'),
     ECPrivateKeyASN = KeyEncoder.ECPrivateKeyASN,
     SubjectPublicKeyInfoASN = KeyEncoder.SubjectPublicKeyInfoASN,
+    BN = require('bn.js')
