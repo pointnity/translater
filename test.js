e30dde4d581d4c79712f927d6dd944c34aeaa146
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-06-09 19:43:13 +0800

    Update test.js

--- test.js
+++ test.js
@@ -7,3 +7,4 @@ var test = require('tape'),
 var keys = {
     rawPrivate: '844055cca13efd78ce79a4c3a4c5aba5db0ebeb7ae9d56906c03d333c5668d5b',
     rawPublic: '04147b79e9e1dd3324ceea115ff4037b6c877c73777131418bfb2b713effd0f502327b923861581bd5535eeae006765269f404f5f5c52214e9721b04aa7d040a75',
+    pemPrivate: '-----BEGIN EC PRIVATE KEY-----\n' +
