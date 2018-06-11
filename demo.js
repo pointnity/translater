95de76470376db44de2f48f32bd0143cfe2145f2
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-06-11 18:42:30 +0800

    Update demo.js

--- demo.js
+++ demo.js
@@ -5,3 +5,6 @@ var rawPrivateKey = '844055cca13efd78ce79a4c3a4c5aba5db0ebeb7ae9d56906c03d333c56
     rawPublicKey = '04147b79e9e1dd3324ceea115ff4037b6c877c73777131418bfb2b713effd0f502327b923861581bd5535eeae006765269f404f5f5c52214e9721b04aa7d040a75'
 
 var EC = require('elliptic').ec
+var encoderOptions = {
+    curveParameters: [1, 3, 132, 0, 10],
+    privatePEMOptions: {label: 'EC PRIVATE KEY'},
